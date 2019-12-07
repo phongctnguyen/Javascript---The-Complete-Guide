7384921596b11ab7296d229ed26d1710dfd06b65
Nguyen Cao Truong Phong <phongctnguyen@gmail.com>
2019-12-08 02:54:22 +0700

    create element

--- Code/4. Working in the DOM/app.js
+++ Code/4. Working in the DOM/app.js
@@ -1,17 +1,29 @@
-const h1 = document.getElementById('main-title');
+// const h1 = document.getElementById('main-title');
 
-h1.textContent = 'Some new title!';
-h1.style.color = 'white';
-h1.style.backgroundColor = 'black';
+// h1.textContent = 'Some new title!';
+// h1.style.color = 'white';
+// h1.style.backgroundColor = 'black';
 
-const li = document.querySelector('li:last-of-type');
-li.textContent = li.textContent + ' (Changed!)';
+// const li = document.querySelector('li:last-of-type');
+// li.textContent = li.textContent + ' (Changed!)';
 
-const body = document.body;
+// const body = document.body;
 
-const listItemElements = document.querySelectorAll('li');
-// const listItemElements = document.getElementsByTagName('li');
+// const listItemElements = document.querySelectorAll('li');
+// // const listItemElements = document.getElementsByTagName('li');
 
-for (const listItemEl of listItemElements) {
-    console.dir(listItemEl);
-}
+// for (const listItemEl of listItemElements) {
+//     console.dir(listItemEl);
+// }
+
+const section = document.querySelector('section');
+const button = document.querySelector('button');
+
+// section.style.backgroundColor = 'blue';
+// section.className = '';
+section.className = 'red-bg';
+
+button.addEventListener('click', () => {
+    // section.classList.toggle('visible')
+    section.classList.toggle('invisible')
+})
